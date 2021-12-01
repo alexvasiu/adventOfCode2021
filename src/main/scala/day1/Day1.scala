9ed4b5291a41ff2b85ae922552d9100c1539b0d2
Alexandru Vasiu <alexandru.vasiu@bolt.eu>
2021-12-01 17:25:12 +0200

    Optimise day 01

--- src/main/scala/day1/Day1.scala
+++ src/main/scala/day1/Day1.scala
@@ -3,21 +3,13 @@ package day1
 import scala.io.Source
 
 object Day1 {
-  def getData: List[Int] =
-    Source.fromResource("day1_input").getLines().map(_.toInt).toList
-
   def countIncreases(measurements: List[Int], offset: Int): Int =
     (0 until measurements.length - offset)
-      .map(index => if measurements(index + offset) > measurements(index) then 1 else 0)
-      .sum
-
-  def solve1(): Unit =
-    println(countIncreases(getData, 1))
-
-  def solve2(): Unit =
-    println(countIncreases(getData, 3))
+      .count(index => measurements(index + offset) > measurements(index))
 
   def main(args: Array[String]): Unit =
-    solve1()
-    solve2()
+    val measurements = Source.fromResource("day1_input").getLines().map(_.toInt).toList
+
+    println(countIncreases(measurements, 1))  // Part 1
+    println(countIncreases(measurements, 3))  // Part 2
 }
